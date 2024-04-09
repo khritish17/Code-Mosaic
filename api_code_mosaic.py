878dc1643bd2da2cec3805f11db73d1fc519ce35
Khritish Kumar Behera <khritish.official@gmail.com>
2024-04-09 16:11:06 +0530

    some new colour coding changes made

--- api_code_mosaic.py
+++ api_code_mosaic.py
@@ -5,4 +5,4 @@ def generate_image(location="", height=500, width=1000, font_size=20, spacing=15
     CM.CodeMosaic(height=height, width=width, font_size=font_size, spacing=spacing, location=location)
 
 # Example usage to generate an image with custom parameters
-generate_image(location=r"D:\Codes\Projects\Code Mosaic\test\test.py", width=1500, font_size=30)
+# generate_image(location=r"D:\Codes\Projects\Code Mosaic\test\test.py", width=1500, font_size=30)
--- color_generator.py
+++ color_generator.py
@@ -1,6 +1,7 @@
 def vibrant_color(token):
     # Split the token into parts
     token = str(token).split(".")
+    # print(token)
 
     # Check if the token belongs to the "Keyword" category
     if token[1] == "Keyword":
@@ -18,10 +19,11 @@ def vibrant_color(token):
             if token[2] == "String":
                 return (25, 249, 216)  # Return a specific color
             # Check if it is a "Number" Literal
-            if token[2] == "Number":
-                return (255, 184, 108)  # Return a specific color
+            # if token[2] == "Number":
+            #     return (255, 184, 108)  # Return a specific color
         except:
-            return (255, 255, 255)  # Return a default color
+            return (255, 184, 108)
+            # return (255, 255, 255)  # Return a default color
 
     # Check if the token belongs to the "Comment" category
     elif token[1] == "Comment":
@@ -33,7 +35,24 @@ def vibrant_color(token):
             if token[2] == "PreprocFile":
                 return (109, 238, 240)  # Return a specific color
         except:
+            return (103, 107, 121)
             return (255, 255, 255)  # Return a default color
-
+    elif token[1] == "Text":
+        return (0, 207, 255)
+    elif token[1] == "Name":
+        try:
+            if token[2] == "Function":
+                return (255, 184, 108)
+                # return (255, 64, 8)
+            if token[2] == "Builtin":
+                return (255, 64, 8)
+        except:
+            pass
+        # return (255, 184, 108)
+        return (255, 255, 255)
+    elif token[1] == "Operator":
+        return (164, 250, 0)
+    elif token[1] == "Punctuation":
+        return (56, 109, 255)
     else:
         return (255, 255, 255)  # Return a default color if no category matches
